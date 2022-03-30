ee5a2fe541261aaf01178ab69527ebfe336104e1
PietroFornari <100316910+PietroFornari@users.noreply.github.com>
2022-03-30 20:48:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0, b=0, area=0;
+    int opzione=0;
+    cin >> a >> b >> opzione;
+    switch(opzione) {
+        case 0:
+            area = (a*b)/2;
+            cout << area << endl;
+            break;
+        case 1:
+            area = (a*a);
+            cout << area << endl;
+            break;
+        case 2:
+            area = (a*b);
+            cout << area << endl;
+            break;
+        default:
+            cout << "opzione non valida\n" << endl;
+    }
+    return 0;
 }
